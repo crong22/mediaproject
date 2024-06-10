1d7b708be08bc704b293e12cfb157b6f5ab7f072
yeonwoo <mamen7624@gmail.com>
2024-06-10 22:05:11 +0900

    Initial Commit

---
+++ mediaproject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mediaproject
+//
+//  Created by 여누 on 6/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
